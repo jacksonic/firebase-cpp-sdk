8a6da96456fe4165410fd9c9be18681ff96ca824
Denver Coneybeare <dconeybe@google.com>
2022-10-08 22:12:10 -0400

    Add ObjectArena C++ skeleton

---
+++ firestore/src/android/object_arena_android.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FIREBASE_FIRESTORE_SRC_ANDROID_OBJECT_ARENA_ANDROID_H_
+#define FIREBASE_FIRESTORE_SRC_ANDROID_OBJECT_ARENA_ANDROID_H_
+
+#include "firestore/src/jni/jni_fwd.h"
+#include "firestore/src/jni/object.h"
+#include "firestore/src/jni/ownership.h"
+
+namespace firebase {
+namespace firestore {
+
+class ObjectArena : public jni::Object {
+ public:
+  using jni::Object::Object;
+
+  static void Initialize(jni::Loader& loader);
+
+  static jni::Local<ObjectArena> Create(jni::Env&);
+
+  int32_t Add(jni::Env&, const jni::Object&) const;
+
+  void Remove(jni::Env&, int32_t) const;
+
+  jni::Local<Object> Get(jni::Env&, int32_t) const;
+};
+
+}  // namespace firestore
+}  // namespace firebase
+
+#endif  // FIREBASE_FIRESTORE_SRC_ANDROID_OBJECT_ARENA_ANDROID_H_
