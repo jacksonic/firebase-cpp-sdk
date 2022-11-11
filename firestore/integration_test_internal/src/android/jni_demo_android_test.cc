1998f0922bfe99a7694f3552908342022e9eb53b
Denver Coneybeare <dconeybe@google.com>
2022-11-11 09:59:45 -0500

    jni_demo_android_test.cc added

---
+++ firestore/integration_test_internal/src/android/jni_demo_android_test.cc
@@ -0,0 +1,17 @@
+#include "firestore/src/android/jni_demo_android.h"
+
+#include "firestore/src/android/jni_demo_android.h"
+
+#include "gtest/gtest.h"
+
+namespace firebase {
+namespace firestore {
+namespace {
+
+TEST(JniDemo, Load) {
+
+}
+
+}  // namespace
+}  // namespace firestore
+}  // namespace firebase
