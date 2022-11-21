f0e94276a02441b30b711c5817d371f59a448ee6
Denver Coneybeare <dconeybe@google.com>
2022-11-21 14:13:09 -0500

    Add absl to include directory of Firestore Android builds

---
+++ firestore/abseil-cpp.cmake
@@ -0,0 +1,38 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+cmake_minimum_required(VERSION 3.6 FATAL_ERROR)
+
+project(abseil-cpp)
+
+include(ExternalProject)
+
+set(ABSL_COMMIT_ISH 20211102.0)
+
+ExternalProject_Add(
+  abseil-cpp
+
+  DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
+  DOWNLOAD_NAME abseil-cpp-${ABSL_COMMIT_ISH}.tar.gz
+  URL https://github.com/abseil/abseil-cpp/archive/${ABSL_COMMIT_ISH}.tar.gz
+  URL_HASH SHA256=dcf71b9cba8dc0ca9940c4b316a0c796be8fab42b070bb6b7cab62b48f0e66c4
+
+  PREFIX ${CMAKE_CURRENT_BINARY_DIR}
+
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
+  TEST_COMMAND ""
+  HTTP_HEADER "${EXTERNAL_PROJECT_HTTP_HEADER}"
+)
